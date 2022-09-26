eb58b71ca821688c5902d5882733d270a3bbda81
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-26 15:21:39 +0100

    test/fix: App.test.js
    Remove innefective test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,5 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../components/App";
-import mockApiResponse from "./mockApiResponse.json";
-import nock from "nock";
 
 describe("App", () => {
   it("matches snapshot", () => {
@@ -20,27 +18,5 @@ describe("App", () => {
     render(<App />);
 
     expect(screen.getByRole("form")).toBeInTheDocument();
-  });
-
-  it("displays SearchResults component on successful search", async () => {
-    render(<App />);
-    nock("http://images-api.nasa.gov/")
-      .defaultReplyHeaders({
-        "access-control-allow-origin": "*",
-        "access-control-allow-credentials": "true",
-      })
-      .get("/search")
-      .query({ q: "moon" })
-      .reply(200, mockApiResponse);
-
-    const searchInput = screen.getByRole("textbox");
-    const button = screen.getByRole("button");
-
-    fireEvent.change(searchInput, { target: { value: "moon" } });
-    fireEvent.click(button);
-
-    const searchResults = await screen.findByTestId("search-results");
-
-    expect(searchResults).toBeInTheDocument();
   });
 });
