953bbbfb5db3503717779ab1a4198b17d2115503
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-26 14:46:47 +0100

    refactor: App
    Adds NASA logo svg to project and imports into App component

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,14 @@ import React, { useState } from "react";
 import "../styles/app.css";
 import Search from "./Search";
 import SearchResults from "./SearchResults";
+import logo from "../assets/img/NASA_Worm_logo.svg";
 
 const App = () => {
   const [results, setResults] = useState([]);
   return (
     <div className="app">
       <header>
-        <img
-          className="nasa-logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/a/a3/NASA_Worm_logo.svg"
-          alt="nasa logo"
-        />
+        <img className="nasa-logo" src={logo} alt="nasa logo" />
         <Search setSearchResults={setResults} />
       </header>
       <SearchResults results={results} />
